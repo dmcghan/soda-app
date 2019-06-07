a1ba835eac62bf5ade2b10b69806bc0817489c35
Dan McGhan <dan.mcghan@oracle.com>
2019-06-07 11:16:24 -0400

    Removed hard-coded service name

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   todoPool: {
     user: 'todo_soda',
     password: 'SecretP@ssw0rd',
-    connectString: 'testdb_tp', // service name in /db_credentials/tnsnames.ora
+    connectString: '[SERVICE_NAME]', // service name in /db_credentials/tnsnames.ora
     poolMin: 4,
     poolMax: 4,
     poolIncrement: 0
