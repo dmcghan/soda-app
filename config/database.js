84d83579acfa8eb78ec7b89081cde7f144aa0a98
Dan McGhan <dan.mcghan@oracle.com>
2019-08-12 17:32:02 -0400

    minor updates

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   todoPool: {
     user: 'todo_soda',
     password: 'SecretP@ssw0rd',
-    connectString: '[SERVICE_NAME]', // service name in /db_credentials/tnsnames.ora
+    connectString: 'tododb_tp', // service name in /db_credentials/tnsnames.ora
     poolMin: 4,
     poolMax: 4,
     poolIncrement: 0
